5355b1a797c17c1a02ca11e37b45d9255d90b8f2
GAURAV NAGALAPURAM VENKATARAMANAN <nv.gau16@gmail.com>
2026-04-08 00:12:31 -0700

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,16 @@
+#define pb push_back
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+
+        string res;
+
+        for (int i = 0; i < min(strs[0].size(), strs[strs.size() - 1].size()); i ++) {
+            if (strs[0][i] == strs[strs.size() - 1][i]) res.pb(strs[0][i]);
+            else break;
+        }
+
+        return res;
+    }
+};
