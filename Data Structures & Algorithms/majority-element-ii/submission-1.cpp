4804e0e03903a2fed81e625952433f4c57716aff
GAURAV NAGALAPURAM VENKATARAMANAN <nagalapu@usc.edu>
2026-04-13 04:47:47 -0700

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        if (nums.size() == 1) return nums;
+
+        int num1 = nums[0], num2 = nums[1];
+        int c1 = 0, c2 = 0;
+
+        for(int i = 0; i < nums.size();i ++) {
+            if (num1 == nums[i]) c1 ++;
+            else if (num2 == nums[i]) c2 ++;
+            else {
+                if (c1 == 0) {
+                    c1 = 1;
+                    num1 = nums[i];
+                } else if (c2 == 0) {
+                    c2 = 1;
+                    num2 = nums[i];
+                } else {
+                    c1 --;
+                    c2 --;
+                }
+            }
+        }
+        c1 = 0;
+        c2 = 0;
+        for (int i : nums) {
+            if (i == num1) c1 ++;
+            else if(i == num2) c2 ++;
+        }
+
+        vector<int> res;
+        if (c1 > nums.size() / 3) res.push_back(num1);
+        if (c2 > nums.size() / 3) res.push_back(num2);
+
+        return res;
+    }   
+};
