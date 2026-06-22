94f545f6a752e23c875ec685ef74a5e61af9bd3b
GAURAV NAGALAPURAM VENKATARAMANAN <nagalapu@usc.edu>
2026-06-21 19:30:27 -0700

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArray(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)):
+            j = i - 1
+            key = arr[i]
+
+            while j >= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            
+            arr[j+1] = key
+        return arr
